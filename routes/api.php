640d039284a6564a36804c58ca8ac6fa39b52a21
Marco De Felice <=>
2024-04-18 16:48:01 +0200

    update composer

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,11 @@
  */
 $app->get('/check', \Budgetcontrol\Authtentication\Controller\AuthController::class . ':check');
 $app->get('/user-info', \Budgetcontrol\Authtentication\Controller\AuthController::class . ':authUserInfo');
-$app->post('/sign-up', \Budgetcontrol\Authtentication\Controller\SignUpController::class . ':signUp');
+$app->post('/register', \Budgetcontrol\Authtentication\Controller\SignUpController::class . ':signUp');
+$app->get('/confirm/{token}', \Budgetcontrol\Authtentication\Controller\AuthController::class . ':confirmToken');
+$app->post('/authenticate', \Budgetcontrol\Authtentication\Controller\AuthController::class . ':authenticate');
+$app->post('/reset-password', \Budgetcontrol\Authtentication\Controller\AuthController::class . ':resetPassword');
+$app->post('/verify-email', \Budgetcontrol\Authtentication\Controller\AuthController::class . ':verifyEmail');
+$app->put('/reset-password/{token}', \Budgetcontrol\Authtentication\Controller\AuthController::class . ':recoveryToken');
+$app->get('/authenticate/{provider}', \Budgetcontrol\Authtentication\Controller\AuthController::class . ':authenticateProvider');
+$app->get('/token', \Budgetcontrol\Authtentication\Controller\AuthController::class . ':token');
